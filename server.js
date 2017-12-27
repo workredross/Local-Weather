befa81f77c2311bc01c1602de1f4b1cfff698259
Kanishk Srivastava <workredross@gmail.com>
2017-12-27 18:24:51 +0530

    added server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 const PORT = process.env.PORT || 3001;
 
 //Heroku
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     next();
+//   } else {
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// });
 
 app.use(express.static('public'));
 
